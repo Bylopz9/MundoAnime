1d99634fe39e7f6fd43538b1c7cb386e72f89845
Bylopz9 <lopz_9@outlook.com>
2020-06-24 20:44:10 -0500

    Primer Proyecto Django. Contiene los Templates

---
+++ webmundo/webapp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request,"webapp/home.html")
+
+def anime(request):
+    return render(request,"webapp/anime.html")
+
+def emision(request):
+    return render(request,"webapp/emision.html")
+
+def genero(request):
+    return render(request,"webapp/genero.html")
+
+def tienda(request):
+    return render(request,"webapp/tienda.html")
+
+def contacto(request):
+    return render(request,"webapp/contacto.html")
+
+def blog(request):
+    return render(request,"webapp/blog.html")
+
+#Video 32
